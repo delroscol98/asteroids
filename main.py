d2d219c00f1e073cfdb7ce89de33513b60d4655f
delroscol98 <collindelrosario98@gmail.com>
2025-08-09 21:41:46 +1000

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from asteroids!")
+# this allows us to use code from
+# the open-source pygame libary
+# throughout this file
 
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+# this line ensures the main() function
+# is only called when this file is
+# run directly; it won't run if it's 
+# imported as a module. It's considered the
+# "pythonic" way tostructure an executable
+# program in Python
 
 if __name__ == "__main__":
     main()
