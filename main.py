bd0ef0d7513156ab0154909461e1da8ec305e1cb
delroscol98 <collindelrosario98@gmail.com>
2025-08-09 22:02:09 +1000

    modified main.py and added constants.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # this allows us to use code from
 # the open-source pygame libary
 # throughout this file
-
 import pygame
+
+# import everything from the module
+# constants.py into the current file
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 # this line ensures the main() function
 # is only called when this file is
@@ -13,6 +18,5 @@ def main():
 # imported as a module. It's considered the
 # "pythonic" way tostructure an executable
 # program in Python
-
 if __name__ == "__main__":
     main()
