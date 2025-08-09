f204fdd9184f01ab15802c87792e7575daf02db0
delroscol98 <collindelrosario98@gmail.com>
2025-08-10 04:10:33 +1000

    modified main.py: Use pygame to get a new GUI window

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
 # this line ensures the main() function
 # is only called when this file is
 # run directly; it won't run if it's 
@@ -19,4 +29,4 @@ def main():
 # "pythonic" way tostructure an executable
 # program in Python
 if __name__ == "__main__":
-    main()
+   main()
