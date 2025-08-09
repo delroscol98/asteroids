2382a95d72cd37620d47499fc728fbf4b7dacc94
delroscol98 <collindelrosario98@gmail.com>
2025-08-10 04:33:27 +1000

    modified main.py and added circleshapy.py

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ import pygame
 # constants.py into the current file
 from constants import *
 
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,13 +36,16 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 # this line ensures the main() function
 # is only called when this file is
